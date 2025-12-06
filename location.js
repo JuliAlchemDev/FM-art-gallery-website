843a9c41284fe2a1f277c8f76ec151d627156910
JuliAlchemDev <julialchem@gmail.com>
2025-12-06 11:54:29 +0100

    feat(location): implement Leaflet map integration and enhance header link styles

---
+++ location.js
@@ -0,0 +1,18 @@
+const map = L.map("map").setView([41.481, -71.31], 15);
+// 99 KING STREET Newport RI 02840 United States of America
+// 41.481511376653835, -71.31036273125875
+// https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png
+// https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}
+L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png", {
+  maxZoom: 19,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>',
+}).addTo(map);
+
+const iconLocation = L.icon({
+  iconUrl: "./assets/icons/navigation/icon-location.svg",
+
+  iconSize: [66, 88],
+});
+const marker = L.marker([41.481511376653835, -71.31036273125875], {
+  icon: iconLocation,
+}).addTo(map);
