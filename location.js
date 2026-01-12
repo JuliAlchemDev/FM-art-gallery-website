894e3683ba40c4c1cda284582c5f45524f72cdf2
Julia Alkhimova <devalchem@gmail.com>
2026-01-12 14:33:55 +0100

    fix(location.js): change map tile source to resolve 401 error

--- location.js
+++ location.js
@@ -3,9 +3,15 @@ const map = L.map("map").setView([41.481, -71.31], 15);
 // 41.481511376653835, -71.31036273125875
 // https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png
 // https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}
-L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png", {
+// L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png", {
+//   maxZoom: 19,
+//     attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>',
+// }).addTo(map);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>',
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
 const iconLocation = L.icon({
